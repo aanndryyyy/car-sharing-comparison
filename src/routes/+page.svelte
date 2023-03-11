8f1ca36a3e74150b0a5153caeec1066d96da7110
Andry Pedak <andry.pedak@gmail.com>
2023-03-11 17:43:40 +0000

    Improve planner

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,40 +16,105 @@
     return firstPer - secPer;
   });
 
-  $: formattedDuration = formatDuration(totalDurationInMinutes);
-
   $: totalMinutes = breakdownMinutes( totalDurationInMinutes ).minutes;
   $: totalHours   = breakdownMinutes( totalDurationInMinutes ).hours;
-  $: totalDays    = breakdownMinutes( totalDurationInMinutes ).days;
+  $: totalDays    = breakdownMinutes( totalDurationInMinutes ).days || 0;
+
+  function setManuallyMinutes( input: Event ) {
+
+    const target = input.target as HTMLInputElement;
+    let value    = target.valueAsNumber;
+    
+    if ( isNaN( value ) ) {
+      value = 0;
+    }
+    
+    totalDurationInMinutes -= breakdownMinutes( totalDurationInMinutes ).minutes;
+    totalDurationInMinutes += value;
+  }
+
+  function setManuallyHours( input: Event ) {
+
+    const target = input.target as HTMLInputElement;
+    let value    = target.valueAsNumber;
+    
+    if ( isNaN( value ) ) {
+      value = 0;
+    }
+    
+    totalDurationInMinutes -= breakdownMinutes( totalDurationInMinutes ).hours*60;
+    totalDurationInMinutes += value*60;
+  }
+
+  function setManuallyDays( input: Event ) {
+
+    const target = input.target as HTMLInputElement;
+    let value    = target.valueAsNumber;
+    
+    if ( isNaN( value ) ) {
+      value = 0;
+    }
+    
+    totalDurationInMinutes -= breakdownMinutes( totalDurationInMinutes ).days*24*60;
+    totalDurationInMinutes += value*24*60;
+
+    console.log( breakdownMinutes( totalDurationInMinutes ) );
+    
+  }
 </script>
 
 <header class="max-w-4xl mx-auto my-4 mb-16 lg:mt-24">
   <h1 class="mb-2 text-5xl font-bold">Car Sharing Comparison</h1>
   
   <small class="text-gray-500">
-    By <a href="http://pedak.me" target="_blank" rel="noreferrer" class="hover:underline">Andry Pedak</a> | <a href="https://github.com/aanndryyyy/bolt-calculator" target="_blank" rel="noreferrer" class="hover:underline">GitHub</a>
+    By <a href="http://pedak.me" target="_blank" rel="noreferrer" class="hover:underline">Andry Pedak</a> | <a href="https://github.com/aanndryyyy/car-sharing-comparison" target="_blank" rel="noreferrer" class="hover:underline">GitHub</a>
   </small>
 </header>
 
 <main class="grid gap-8 md:grid-cols-3 max-w-4xl mx-auto px-4 lg:px-0 my-4 mb-16 lg:my-8">
 
   <aside class="md:col-span-1">
-    <h2 class="text-3xl mb-6">Planner</h2>
+    <div class="mb-6">
+      <h2 class="text-3xl">Planner</h2>
+    
+      <small class="text-gray-500 mb-6">
+        Use sliders or enter manually
+      </small>
+    </div>
 
-    <label class="block mb-4">
+    <label class="block mb-6">
+      <span class="block mb-2">Duration</span>
+
       <div class="flex-col sm:flex-row flex gap-2">
-        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type=number bind:value={totalDays} min=0 max=99 pattern="[0-9]">
-        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type=number bind:value={totalHours} min=0 max=99 pattern="[0-9]">
-        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type=number bind:value={totalMinutes} min=0 max=99 pattern="[0-9]">
-        <!-- <input class="appearance-none block w-full disabled:bg-gray-200 disabled:text-gray-500 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type=text bind:value={formattedDuration} min=0 disabled> -->
+        
+        <div class="relative">
+          <input class="appearance-text block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type=number on:change={setManuallyDays} bind:value={totalDays} min=0>
+          <span class="absolute leading-none text-gray-500 text-[10px] font-bold right-4 top-2/4 -translate-y-2/4">{ totalDays === 1 ? 'DAY' : 'DAYS' }</span>
+        </div>
+        
+        <div class="relative">
+          <input class="appearance-text block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type=number on:change={setManuallyHours} bind:value={totalHours} min=0>
+          <span class="absolute leading-none text-gray-500 text-[10px] font-bold right-4 top-2/4 -translate-y-2/4">{ totalHours === 1 ? 'HOUR' : 'HRS' }</span>
+        </div>
+        
+        <div class="relative">
+          <input class="appearance-text block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type=number on:change={setManuallyMinutes} bind:value={totalMinutes} min=0>
+          <span class="absolute leading-none text-gray-500 text-[10px] font-bold right-4 top-2/4 -translate-y-2/4">{ totalMinutes === 1 ? 'MIN' : 'MINS' }</span>
+        </div>
       </div>
-      <input tabindex="-1" class="w-full h-2 bg-green-600 rounded-lg appearance-none cursor-pointer" type=range bind:value={totalDurationInMinutes} min=0 max=4320>
+
+      <input tabindex="-1" class="w-full h-2 bg-green-600 rounded-lg appearance-none cursor-pointer" type=range bind:value={totalDurationInMinutes} min=0 max=10080>
     </label>
     
     <label class="block mb-8">
-      <span class="block mb-1">Kilometres</span>
-      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type=number bind:value={totalKilometres} min=0>
-      <input tabindex="-1" class="w-full h-2 bg-green-600 rounded-lg appearance-none cursor-pointer" type=range bind:value={totalKilometres} min=0 max=360>
+      <span class="block mb-2">Kilometres</span>
+
+      <div class="relative">
+        <input class="appearance-text block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type=number bind:value={totalKilometres} min=0>
+        <span class="absolute leading-none text-gray-500 text-[10px] font-bold right-4 top-2/4 -translate-y-2/4">KM</span>
+      </div>
+
+      <input tabindex="-1" class="w-full h-2 bg-green-600 rounded-lg appearance-none cursor-pointer" type=range bind:value={totalKilometres} min=0 max=2500>
     </label>
   </aside>
 
@@ -59,7 +124,7 @@
       <h2 class="text-3xl">Cars</h2>
 
       <small class="text-gray-500">
-        Prices don't match? <a href="https://github.com/aanndryyyy/bolt-calculator/blob/main/src/cars.ts" target="_blank" rel="noreferrer" class="underline">Contribute</a>
+        Prices don't match? <a href="https://github.com/aanndryyyy/car-sharing-comparison/blob/main/README.md#data-scraping" target="_blank" rel="noreferrer" class="underline">Contribute</a>
       </small>
     </div>
 
@@ -68,12 +133,12 @@
         <div class="flex justify-between items-center p-4 shadow-md rounded-md border border-gray-500" class:border-green-600={i === 0} class:border-2={i === 0}>
           <div>
             <h2 class="font-semibold text-base">{genericCar.getName()}</h2>
-            <!-- <p class="text-gray-500">&euro; {car.price.minute}/<span class="text-xs">min</span> | &euro; {car.price.km}/<span class="text-xs">km</span></p> -->
+            <p class="text-xs text-gray-600">{genericCar.getFormattedMinutePrice()} | {genericCar.getFormattedKilometrePrice()}</p>
           </div>
     
           <div class="text-2xl font-bold text-right" class:text-green-600={i === 0}>
             {genericCar.getFormattedTotalPrice(totalDurationInMinutes, totalKilometres)}
-            <!-- <span class="block text-xs font-normal text-blue-600" title="Long-term rent discount">{get_discount_total(totalMinutes, car).toFixed(0)}&euro;</span> -->
+            <span class="block text-xs font-normal text-blue-600" title="Long-term rent discount">{genericCar.getFormattedLongTermDiscount(totalDurationInMinutes, totalKilometres)}</span>
           </div>
         </div>
       {/each}
