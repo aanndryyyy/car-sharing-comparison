e917b79c81c47de1e3b19b6c4e5a93123418126e
Andry Pedak <andry.pedak@gmail.com>
2023-03-11 23:12:48 +0000

    Fix missing locale

--- src/lib/i18n/index.ts
+++ src/lib/i18n/index.ts
@@ -8,5 +8,5 @@ register( 'et', () => import( '../../locales/et.json' ) );
 
 init({
 	fallbackLocale: defaultLocale,
-	initialLocale: browser ? window.navigator.language : defaultLocale,
+	initialLocale: defaultLocale,
 })
