59498b44d2e7e973de307884914152f3bfabe1b9
Andry Pedak <andry.pedak@gmail.com>
2023-03-11 23:08:57 +0000

    Fix missing language selection

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">  
   import "./page.css";
 
+  import '$lib/i18n';
   import { browser } from '$app/environment'
-  import '$lib/i18n' // Import to initialize. Important :)
   import { locale, waitLocale, isLoading } from 'svelte-i18n'
   import type { LayoutLoad } from './$types'
 
   export const load: LayoutLoad = async () => {
-    if (browser) {
-      locale.set(window.navigator.language)
+    
+    if ( browser && [ 'en-US', 'et' ].includes( window.navigator.language ) ) {
+      locale.set( window.navigator.language )
     }
+
     await waitLocale()
   }
 </script>
