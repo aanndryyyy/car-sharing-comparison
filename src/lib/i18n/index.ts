36a6f90514c655e1c4ed608cdaa222794891b603
Andry Pedak <andry.pedak@gmail.com>
2023-03-12 15:35:50 +0000

    Change loading

--- src/lib/i18n/index.ts
+++ src/lib/i18n/index.ts
@@ -1,10 +1,18 @@
-import { browser } from '$app/environment'
-import { init, register } from 'svelte-i18n'
+// import { browser } from '$app/environment'
+// import { init, register } from 'svelte-i18n'
 
 const defaultLocale = 'en-US'
 
-register( 'en-US', () => import( '../../locales/en-US.json' ) );
-register( 'et', () => import( '../../locales/et.json' ) );
+// register( 'en-US', () => import( '../../locales/en-US.json' ) );
+// register( 'et', () => import( '../../locales/et.json' ) );
+
+import { init, addMessages } from 'svelte-i18n';
+
+import et from '../../locales/et.json';
+import enUS from '../../locales/en-US.json';
+
+addMessages('et', et);
+addMessages('en-US', enUS);
 
 init({
 	fallbackLocale: defaultLocale,
