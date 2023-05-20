6ddddb5fb1496b823f1fad373cdf8c45356a36af
Andry Pedak <andry.pedak@gmail.com>
2023-05-20 18:56:33 +0000

    Location planner initials

--- src/lib/Components/CarsMap.svelte
+++ src/lib/Components/CarsMap.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { _ } from 'svelte-i18n'
+  import { MapContainer } from '$lib/Store/MapContainerStore'
   import BottomSheet from 'svelte-swipeable-sheets/BottomSheet.svelte'
   import Filters from '$lib/Components/Filters.svelte'
 
@@ -7,11 +8,8 @@
 </script>
 
 <div>
-  <!--    <GoogleMap apiKey="your-maps-api-key"/>-->
-  <img
-    src="https://developers.google.com/static/maps/images/landing/hero_maps_static_api_480.png"
-    alt="testing"
-  />
+  <div class="h-96" bind:this={$MapContainer} />
+  
   <button
     class="h-10 w-full border-gray-300 border-2 block md:hidden"
     on:click={() => (open = true)}
