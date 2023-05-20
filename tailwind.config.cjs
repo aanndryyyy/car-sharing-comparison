bad8ec24dc2cacaac7a2ce69daf3ef2d57532661
Andry Pedak <andry.pedak@gmail.com>
2023-05-20 19:03:49 +0000

    Add Inter font from Tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors')
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
-      colors: {
-        gray: colors.gray,
-        green: {
-          DEFAULT: "#60B941",
-          light: "#7AD959"
-        }
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     }
   },
