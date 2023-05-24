10f5e07eb36074da6206f94c5f73145e7e8659e4
melliv <villem.madisson883@gmail.com>
2023-05-24 23:39:07 +0300

    Car image update beast

--- src/lib/Car/BeastCar.ts
+++ src/lib/Car/BeastCar.ts
@@ -6,6 +6,13 @@ import type { SvelteComponent } from 'svelte'
 import BeastCarPopover from '$lib/Popovers/BeastCarPopover.svelte'
 import type { ICarBeast } from '$lib/Types/Interfaces/ICarBeast'
 import { Provider } from '../Types/Enums/Provider'
+import No_image_available from '$lib/Images/no-image-available.png'
+import Tesla_Model_3_Long_Range from '$lib/Images/Providers/Beast/Cars/Tesla-Model-3-Long-Range.png'
+import Tesla_Model_3_Standard_Range from '$lib/Images/Providers/Beast/Cars/Tesla-Model-3-Standard-Range.png'
+import Tesla_Model_3_Performance from '$lib/Images/Providers/Beast/Cars/Tesla-Model-3-Performance.png'
+import Tesla_Model_X_Performance from '../Images/Providers/Beast/Cars/Tesla-Model-X-Performance.png'
+import Tesla_Model_Y_Long_Range from '$lib/Images/Providers/Beast/Cars/Tesla-Model-Y-Long-Range.png'
+import Tesla_Model_S from '$lib/Images/Providers/Beast/Cars/Tesla-Model-S.png'
 
 class BeastCar implements BaseCar {
   readonly carData: ICarBeast
@@ -87,8 +94,26 @@ class BeastCar implements BaseCar {
   }
 
   getCarImg(): string {
-    // temp
-    return 'https://citybee.ee/wp-content/uploads/2021/08/CityBee_ToyotaYaris_EE-1024x605.png'
+    switch (this.carData.name) {
+      case "Model 3 Standard Range":
+        return Tesla_Model_3_Standard_Range
+      case "Model 3 Long Range":
+        return Tesla_Model_3_Long_Range
+      case "Model 3 Performance":
+        return Tesla_Model_3_Performance
+      case "Model Y Long Range":
+        return Tesla_Model_Y_Long_Range
+      case "Model S Standard Range":
+        return Tesla_Model_S
+      case "Model Y Performance":
+        return Tesla_Model_Y_Long_Range
+      case "Model S Performance":
+        return Tesla_Model_S
+      case "Model X Performance":
+        return Tesla_Model_X_Performance
+      default:
+        return No_image_available
+    }
   }
 
   /**
