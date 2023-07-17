8120175a4dfa93f4103c2ec6cd717d03b19b7276
melliv <villem.madisson883@gmail.com>
2023-07-17 17:18:50 +0300

    Analytics data domain updated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -43,12 +43,6 @@
   <meta name="twitter:image" content={ogImage} />
   <meta name="twitter:card" content="summary_large_image" />
 
-  <script
-    defer
-    data-domain="aanndryyyy.github.io/car-sharing-comparison"
-    src="https://plausible.io/js/script.outbound-links.js"
-  ></script>
-
   {@html `<script>(g => { var h, a, k, p = "The Google Maps JavaScript API", c = "google", l = "importLibrary", q = "__ib__", m = document, b = window; b = b[c] || (b[c] = {}); var d = b.maps || (b.maps = {}), r = new Set, e = new URLSearchParams, u = () => h || (h = new Promise(async (f, n) => { await (a = m.createElement("script")); e.set("libraries", [...r] + ""); for (k in g) e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]); e.set("callback", c + ".maps." + q); a.src = \`https://maps.\$\{c\}apis.com/maps/api/js?\` + e; d[q] = f; a.onerror = () => h = n(Error(p + " could not load.")); a.nonce = m.querySelector("script[nonce]")?.nonce || ""; m.head.append(a); })); d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n)); })({key: "${PUBLIC_GOOGLE_API_KEY}",v: "beta",});</script>`}
 </svelte:head>
 
