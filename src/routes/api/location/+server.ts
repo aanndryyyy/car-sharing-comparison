faf265b491d51363c44bc5a0179d116ba24f9cba
Andry Pedak <andry.pedak@gmail.com>
2023-09-17 19:00:47 +0000

    Run on the edge

--- src/routes/api/location/+server.ts
+++ src/routes/api/location/+server.ts
@@ -1,5 +1,9 @@
 import { SECRET_BACKEND_BASE_URL } from '$env/static/private'
 
-export const GET = async () => {
+export const config = {
+  runtime: 'edge',
+}
+
+export const GET = async ({ fetch }) => {
   return await fetch(SECRET_BACKEND_BASE_URL + 'location')
 }
